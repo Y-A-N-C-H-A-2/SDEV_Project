c879cc80e878785af56e2af3b0b1fb1e1922ca97
Y-A-N-C-H-A-2 <c24490782@mytudublin.ie>
2026-03-06 20:27:47 +0000

    Move Heroku config files to repo root and update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,12 @@
 """
 WSGI entry point for Heroku deployment
 """
+import sys
+import os
+
+# Add CrossPaths to Python path
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'CrossPaths'))
+
 from app import create_app
 
 app = create_app()
